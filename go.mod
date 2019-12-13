09c5c7d5a0e1793ff34bd54bc00aaa7df4040a95
hakuna <kwangmin010@gmail.com>
2019-12-13 14:49:53 +0900

    ..

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-xorm/xorm v0.7.9
 	github.com/jinzhu/gorm v1.9.11
 	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/labstack/gommon v0.3.0
+	github.com/mattn/go-sqlite3 v1.11.0
 	github.com/valyala/fasttemplate v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
 )
--- repo/model/model.go
+++ repo/model/model.go
@@ -1,29 +1,38 @@
 package model
 
 import (
+	"encoding/json"
 	"github.com/jinzhu/gorm"
 )
 
 type User struct {
-	gorm.Model `json:"-"`
+	gorm.Model
 	Email      string    `json:"email" gorm:"type:varchar(100);unique_index"`
 	Name       string    `json:"name"`
 	Password   string    `json:"password"`
-	Role       string    `json:"-" gorm:"size:255"`
-	Article    []Article `json:"articles" gorm:"foreignkey:Email"`
-	Auth       Auth      `json:"-" gorm:"foreignkey:Email"`
+	Role       string    `gorm:"size:255"`
+	Article    []Article `json:"articles"`
+	Auth       *Auth      `gorm:"foreignkey:Email"`
+}
+
+func (u User) ToString() string {
+	data, err := json.MarshalIndent(&u, "", "	")
+	if err != nil {
+		return err.Error()
+	}
+	return string(data)
 }
 
 type Article struct {
-	gorm.Model `json:"-"`
-	Email      string `json:"-" gorm:"type:varchar(100);unique_index"`
+	gorm.Model
+	Email      string `gorm:"type:varchar(100)"`
 	Title      string `json:"title"`
 	Body       string `json:"body"`
 }
 
 type Auth struct {
-	gorm.Model   `json:"-"`
-	Email        string `json:"-" gorm:"type:varchar(100);unique_index"`
+	gorm.Model
+	Email        string `gorm:"type:varchar(100)"`
 	AccessToken  string
 	RefreshToken string
 }
